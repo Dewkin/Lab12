256d77b90776328700bac47f1bfeb281b75caee8
Devin Mesenbrink <devin_mesenbrink@hotmail.com>
2023-11-21 22:42:22 -0600

    Update television.py

--- television.py
+++ television.py
@@ -10,55 +10,59 @@ class Television:
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
         self.__previous_volume = Television.MIN_VOLUME
+        self.__volume_display = Television.MIN_VOLUME
 
-    def power(self):
+    def power(self) -> None:
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self) -> None:
         if self.__status:
             if not self.__muted:
                 self.__previous_volume = self.__volume
                 self.__volume = Television.MIN_VOLUME
+                self.__volume_display = Television.MIN_VOLUME
             else:
                 self.__volume = self.__previous_volume
+                self.__volume_display = self.__previous_volume
             self.__muted = not self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> None:
         if self.__status:
             self.__channel = (self.__channel + 1) % (Television.MAX_CHANNEL + 1)
 
-    def channel_down(self):
+    def channel_down(self) -> None:
         if self.__status:
             self.__channel = (self.__channel - 1) % (Television.MAX_CHANNEL + 1)
 
-    def volume_up(self):
+    def volume_up(self) -> None:
         if self.__status:
             if self.__muted:
                 self.__muted = False
                 self.__volume = self.__previous_volume
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
+                self.__volume_display = self.__volume
 
-    def volume_down(self):
+    def volume_down(self) -> None:
         if self.__status:
             if self.__muted:
                 self.__muted = False
                 self.__volume = self.__previous_volume
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
+                self.__volume_display = self.__volume
 
     @property
-    def volume(self):
+    def volume(self) -> int:
         return self.__volume
 
     @property
-    def channel(self):
+    def channel(self) -> int:
         return self.__channel
 
     @property
-    def is_muted(self):
+    def is_muted(self) -> bool:
         return self.__muted
 
-    def __str__(self):
-        volume_display = 0 if self.__muted else self.__volume
-        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {volume_display}'
+    def __str__(self) -> str:
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume_display}'
